4c9da62f5d6e8208f2a8998b8f7f4aebb2b47aca
kryukovD <98350191+kryukovD@users.noreply.github.com>
2024-03-22 17:12:27 +0300

    update 404
    dsada

--- wp-content/themes/geolog/404.php
+++ wp-content/themes/geolog/404.php
@@ -1,24 +1,25 @@
-<?php
-/**
- * The template for displaying 404 pages (not found)
- *
- * @link https://codex.wordpress.org/Creating_an_Error_404_Page
- *
- * @package WordPress
- * @subpackage Twenty_Nineteen
- * @since Twenty Nineteen 1.0
- */
-
+<?
 get_header();
 ?>
-
-	<div id="primary" class="content-area">
+<div id="primary" class="content-area">
 		test
 		<main id="main" class="site-main">
 			<div class="error-404 not-found">
 				<header class="page-header">
 					<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'twentynineteen' ); ?></h1>
 				</header><!-- .page-header -->
+<section class="section-404">
+                <div class="container">
+                    <div class="section-404-wrappper">
+                        <div class="section-404__image">
+                            <img src="/wp-content/themes/geolog/assets/images/404.png" alt="404">
+                        </div>
+                        <div class="section-404__message">
+                            К сожалению, страница не найдена
+                        </div>
+                        <div class="section-404__btn" >
+                            <a class="site-btn-2 site-btn-2_404" href="/">Вернуться на главную </a>
+                        </div>
 
 				<div class="page-content">
 					<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'twentynineteen' ); ?></p>
@@ -28,6 +29,9 @@ get_header();
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
+                    </div>
+                </div>
+            </section>
 
 <?php
 get_footer();
